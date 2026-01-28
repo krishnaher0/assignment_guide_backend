7094876e6cd5499b6c05b9af4c7383cb6daef29b
krishna <krishna613460@gmail.com>
2026-01-29 01:58:33 +0545

    feat(routes): update user management routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,12 +8,13 @@ import {
     updateProfile
 } from '../controllers/userController.js';
 import { protect, authorize } from '../middleware/authMiddleware.js';
+import { updateProfileValidator } from '../validators/authValidators.js';
 
 const router = express.Router();
 
 // User profile routes (Self)
 router.get('/profile', protect, getProfile);
-router.put('/profile', protect, updateProfile);
+router.put('/profile', protect, updateProfileValidator, updateProfile);
 
 // Admin user management routes
 router.get('/developers', protect, authorize('admin'), getDevelopers);
